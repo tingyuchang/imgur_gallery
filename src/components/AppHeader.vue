a2819c9ae9faccff7099aa5642ad70f2b5e31ed0
tingyuchang <tingyugenius@gmail.com>
2020-04-22 21:02:36 +0800

    import semantic UI

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        Image Gallery
+    <div class="ui secondary pointing menu">
+        <a class="active item" href="/">
+            Image Gallery
+        </a>
+        <div class="right menu">
+            <a class="item">Login</a>
+        </div>
     </div>
 </template>
 
