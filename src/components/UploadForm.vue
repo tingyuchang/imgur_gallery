53b2f7af0080e2ea569c456304553a2d91b040f6
tingyuchang <tingyugenius@gmail.com>
2020-04-23 11:27:59 +0800

    finishd uploadform

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,13 +1,48 @@
 <template>
-    <h3>UploadForm</h3>
+    <div class="dragger">
+        <input 
+            type="file"
+            accept="image/*"
+            multiple
+            @change="uploadImages($event.target.files)"
+
+        />
+        <span>Please drag your images here!!!</span>
+    </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
     name: "UploadForm",
+    methods: {
+        ...mapActions([
+            'uploadImages',
+        ]),
+    }
 }
 </script>
 
 <style scoped>
+    .dragger {
+        height: 30vh;
+        border: 2px dashed black;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+    }
+    .dragger:hover {
+        background-color: #eee;
+    }
+    input {
+        height: 30vh;
+        width: 100%;
+        position: absolute;
+        opacity: 0;
+
+    }
 
 </style>
