40e73d8dfac5f56b65a23a20ff58aecdae100e55
tingyuchang <tingyugenius@gmail.com>
2020-04-23 11:17:57 +0800

    finish image list

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -13,12 +13,23 @@ export default {
         window.location = `${K_ROOT_URL}/oauth2/authorize?${qs.stringify(queryString)}`;
     },
     fetchImages(token) {
-        return axios.get(K_ROOT_URL, {
+        return axios.get(`${K_ROOT_URL}/3/account/me/images`, {
             headers: {
                 Authorization: `Bearer ${token}`,
             }
         });
     },
-    uploadImages() {},
+    uploadImages(images, token) {
+        const promises = Array.from(images).map(image => {
+            const formData = new FormData();
+            formData.append('image', image);
+            return axios.post(`${K_ROOT_URL}/3/image`, formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            })
+        });
+        return Promise.all(promises);
+    },
     getFavoriteImages() {},
 };
--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,13 +1,42 @@
 <template>
-    <h3>this is a image list</h3>
+    <div v-if="isLoggedIn" class="image-container">
+        <img v-for="image in allImages" :src="image.link" :key="image.id" />
+    </div>
+    <div v-else>
+        <h3>Please Login</h3>
+    </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+
 export default {
     name: "ImageList",
+    created() {
+        this.fetchImages();
+    },
+    methods: {
+        ...mapActions([
+            'fetchImages'
+        ]),
+    },
+    computed: {
+        ...mapGetters([
+            'allImages',
+            'isLoggedIn',
+        ]),
+    }
 }
 </script>
 
 <style scoped>
+    .image-container {
+        column-count: 3;
+        column-gap: 0;
+    }
+    img {
+        max-width: 100%;
+        padding: 5px;
+    }
 
 </style>
